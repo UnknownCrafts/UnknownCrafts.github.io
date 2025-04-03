297b88cd2462529ba4734295c9fe749e9f25e018
Surya Vasudev <suryavasudev05@gmail.com>
2025-04-03 16:53:44 -0400

    Added CTF thing

--- projects.py
+++ projects.py
@@ -1,4 +1,25 @@
 PROJECTS = [
+    {
+            "date": "2025",
+            "title": "uOCTF", 
+            "description": """Our team achieved **1st place** 🥇 in the uOCTF 2025 Capture the Flag (CTF) competition, organized by the uOttawa Cybersecurity Club. This intense 3-hour event presented 30 challenges spanning Web Exploitation, Forensics, Hardware Security, and Cryptography.
+
+We successfully solved 28 out of 30 challenges, demonstrating a strong grasp of diverse cybersecurity concepts and techniques. As a participant relatively new to CTFs, I found this experience incredibly rewarding, validating my self-taught knowledge from independent study.
+
+**Key Learnings and Achievements:**
+
+* Rapid Problem Solving: Navigating and solving complex security challenges under strict time constraints.
+* Diverse Skill Application: Applying knowledge across various domains, including:
+    * Cryptography: Successfully decrypting and analyzing encoded data.
+    * Forensics: Recovering hidden information from digital artifacts.
+    * Web Exploitation: Identifying and exploiting vulnerabilities in web applications.
+    * Hardware: Understanding and working with hardware related security issues.
+* Technical Discoveries:
+    * Learned advanced techniques for hiding files within ZIP archives, bypassing standard extraction methods.
+    * Gained practical experience in mounting and analyzing virtual flash drive file systems.
+* Team Collaboration: Leveraging teamwork and shared expertise to efficiently tackle challenges.
+"""
+        },
         {
             "date": "2025",
             "title": "QNX FIGHTER (cuHacking 6)", 

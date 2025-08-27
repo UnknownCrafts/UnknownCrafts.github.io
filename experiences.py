18138be22a821de1aabca864178e601198a35bce
Surya Vasudev <suryavasudev05@gmail.com>
2025-08-27 15:37:28 -0400

    My First Job!

--- experiences.py
+++ experiences.py
@@ -1,5 +1,18 @@
 EXPERIENCES = [
     {
+        "date": "Sep 2025-Present",
+        "title": "Full-stack Developer @ CEED",
+        "description": """
+* Website is https://makerepo.com/
+* Ruby and Ruby on Rails
+* Linux server administration: Command line usage, SSH, systemd units, bash scripting.
+* Git version control.
+* JavaScript
+* PostgreSQL database administration
+* Backend server programming and design patterns: MVC Pattern, SQL query fundamentals, REST API design
+            """,
+    },
+    {
         "date": "Feb 2025-Present",
         "title": "ADAM (helloadam.ca)",
         "description": """
